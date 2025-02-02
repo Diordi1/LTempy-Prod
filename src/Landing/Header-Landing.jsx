56dcb6dbb8ecaf20a4aa38361673de9a83b023a3
wasteapp111@gmail.com <Diordi1>
2025-02-02 21:04:25 +0530

    inspire-1

--- src/Landing/Header-Landing.jsx
+++ src/Landing/Header-Landing.jsx
@@ -34,7 +34,7 @@ function HeaderLanding() {
             setsrop(!srop);
 
         }}>&#8801;</p>
-        <div className='sm:hidden absolute z-20 bg-gray-100 top-0 right-0 rounded-lg w-[60%] mt-[19%] me-[14%] p-10 text-black ' hidden={srop}>
+        <div className='sm:hidden absolute z-20 bg-gray-100 top-0 right-0 rounded-lg w-[60%] mt-[19%] me-[14%] p-5 text-black ' hidden={srop}>
             <ul className=''>
                 <li className='text-2xl font-bold mb-3 text-center flex gap-3 items-center border-b-2 pb-2 border-black'><svg class="w-8 h-8 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
--- src/Landing/Partner.jsx
+++ src/Landing/Partner.jsx
@@ -2,8 +2,8 @@ function Partner() {
     return (<div className="sm:h-[30%] h-[20%] text-black py-8">
 
             <div className="flex flex-col items-center w-full ">
-                <p className="text-4xl font-bold">Our Top Partners</p>
-                <div className="flex gap-12 py-9">
+                <p className="sm:text-4xl text-2xl font-bold">Our Top Partners</p>
+                <div className="flex gap-12 sm:py-9 py-4">
 
                 <p ><img className="h-10 object-contain" src="metlablu.png"></img></p>
                 <p ><img className="h-9 object-contain" src="viewio.png"></img></p>
